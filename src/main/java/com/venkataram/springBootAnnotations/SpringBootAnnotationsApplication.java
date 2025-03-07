9f36813d28d44d89ce0a7eb83532c004def9bdb4
venkat10raman <89566802+venkat10raman@users.noreply.github.com>
2025-03-07 19:07:47 +0530

    Update SpringBootAnnotationsApplication.java

--- src/main/java/com/venkataram/springBootAnnotations/SpringBootAnnotationsApplication.java
+++ src/main/java/com/venkataram/springBootAnnotations/SpringBootAnnotationsApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootAnnotationsApplication {
-
+	// https://www.youtube.com/watch?v=myW9bh9iSm4
+	// refer this tutorial
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAnnotationsApplication.class, args);
 	}
